1a177a600bb7ccda7b93eb774bb55ea139c99eaa
kishandholakiya1027 <kishandholakiya1027@gmail.com>
2024-01-16 11:47:58 +0530

    fix: order page

--- src/lib/api-path.ts
+++ src/lib/api-path.ts
@@ -51,10 +51,11 @@ export const apiPath = {
     },
     checkOut: {
       createOrder: "order/create",
-      updateOrder: "order/update"
+      updateOrder: "order/update",
     },
     order: {
-      getOrder: "order/user_order"
+      getOrder: "order/user_order",
+      updatePaymentOrder: "order/update_order",
     }
    
   }
