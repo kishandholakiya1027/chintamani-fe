d280af39cb7ba61d01fb4c2f9f713bfdb878a1b6
Kisha. Dholakiya <kishan.dholakiya@syncortex.com>
2024-01-18 09:31:57 +0530

    added share social

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -9,12 +9,20 @@ import { showToast } from '@/lib/utils'
 import { addCartProduct, setOpenCart } from '@/redux/reducer/cart'
 import { productType } from '@/lib/interfaces/category'
 import TrendingDiamond from '../TrendingDiamond'
+import SocialShare from '../social-share'
+
+
+function Demo() {
+
+}
+
 
 const ProductDetailsComponent = () => {
     const { apiAction } = useApi()
     const { id } = useParams()
     const { cart: { cartProduct }, auth: { user, token } } = useSelector((state: any) => state)
     let cartProductIds = cartProduct?.map((product: any) => product?.product?.id || product?.id)
+    
     const [product, setProduct] = useState<productType>({})
     const [currentImage, setCurrentImage] = useState<any>("")
     const navigate = useNavigate()
@@ -92,22 +100,7 @@ const ProductDetailsComponent = () => {
         { title: "Pavilian Angle", description: product?.pavilian_angle },
     ];
 
-    const shareProducts = [{
-        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/5ed313dc0943d74d1cd06306416d9fd81779fae534cf02fa33dc4f5513d5c062?apiKey=d15e42286684479bbc853a10c1e3f3db&",
-        onclick: () => window.open('https://www.facebook.com', '_blank')
-    }, {
-        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/7a821787d499c1d4b2b958ecb3eb8f936207d4f0db01eef898933d690c461817?apiKey=d15e42286684479bbc853a10c1e3f3db&",
-        onclick: () => window.open('https://www.instagram.com', '_blank')
-    }, {
-        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/49441f90835a0a9740bd0ca50df5288e38eb2c502f43c2706142e8229c22cbd4?apiKey=d15e42286684479bbc853a10c1e3f3db&",
-        onclick: () => window.open('https://www.linkedin.com', '_blank')
-    }, {
-        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/18f336aae467a95bfaf56e2f68bd4a6cfeb85db73018e9714afd54de7af33015?apiKey=d15e42286684479bbc853a10c1e3f3db&",
-        onclick: () => window.open('https://wa.me/1234567890', '_blank')
-    }, {
-        img: "https://cdn.builder.io/api/v1/image/assets/TEMP/a92925048654b5dceaed239e42b7fcf2f7410185e697411358fb1d569b3ba81b?apiKey=d15e42286684479bbc853a10c1e3f3db&",
-        onclick: () => { }
-    }]
+
 
     return (
         <div className="flex flex-col">
@@ -162,23 +155,7 @@ const ProductDetailsComponent = () => {
                 <span className=" text-black text-center text-base font-semibold leading-5 tracking-wider items-stretch border bg-white  mt-2 px-7 py-4 rounded-lg border-solid border-indigo-950 self-start lg:ml-2.5 md:ml-2.5 ml-0 max-md:px-5">
                     Contact
                 </span>
-                <div className=" text-neutral-700 text-2xl font-bold leading-10  mt-10 self-start lg:ml-2.5 md:ml-2.5 ml-0">
-                    Share this product
-                </div>
-                <div className="flex w-[201px] max-w-full items-stretch gap-1  mt-2 self-start lg:ml-2.5 md:ml-2.5 ml-0">
-                    {shareProducts.map((elm, index) => {
-                        return <div onClick={() => elm?.onclick()}>
-                            <img
-                                key={index}
-                                loading="lazy"
-                                src={elm?.img}
-
-                                className="aspect-square object-contain object-center w-full justify-center items-center overflow-hidden shrink-0 flex-1"
-                            />
-
-                        </div>
-                    })}
-                </div>
+                <SocialShare />
                 <div className="text-center">
                     <div className="text-3xl font-bold mt-8 mb-4">Diamond Details</div>
                     <table className="table-auto mx-auto mb-8 border-collapse border border-slate-400 lg:w-[500px] md:w-[500px] w-full">
